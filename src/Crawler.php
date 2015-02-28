b5a78b3c0d8ac6cdff664444a0a90550a9be658a
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-02-28 23:35:22 +0000

    Final Crawler

--- src/Crawler.php
+++ src/Crawler.php
@@ -2,19 +2,55 @@
 
 namespace Simian;
 
+use Simian\Pages\PageBuilder;
+use Simian\Repositories\StorageRepository;
+use GuzzleHttp\Client;
+
 class Crawler
 {
-    private $storage;
+    private $baseUrl;
+    private $asins = [];
 
-    public function setStorageFolder($path)
+    public function __construct($baseUrl, $storagePath)
     {
-        $this->storage = $path;
-
-        return $this;
+        $this->baseUrl = $baseUrl;
+        $this->client = new Client();
+        $this->pageBuilder = new PageBuilder($this->baseUrl);
+        $this->storageRepository = new StorageRepository($storagePath);
     }
 
-    public function run()
+    public function run(array $asins)
     {
-        file_put_contents($this->storage . "B008EOJIVG-" . time() . ".html", "hello");
+        $writtenFiles = [];
+
+        foreach ($asins as $asin) {
+            $page = $this->getPage($asin);
+            $this->asins[$asin] = $page;
+            $filename = $this->storageRepository->add($page);
+
+            $writtenFiles[] = $filename;
+        }
+
+        return $writtenFiles;
+    }
+
+    private function getPage($asin)
+    {
+        $htmlStream = $this->getHtmlStream($asin);
+
+        $page = $this->pageBuilder->setAsin($asin)
+                                  ->setBody($htmlStream)
+                                  ->build();
+
+        return $page;
+    }
+
+    private function getHtmlStream($asin)
+    {
+        $request = $this->client->createRequest('GET', $this->baseUrl . $asin);
+        $response = $this->client->send($request);
+        $bodyStream = $response->getBody(true);
+
+        return $bodyStream;
     }
 }
--- tests/CrawlerTest.php
+++ tests/CrawlerTest.php
@@ -7,20 +7,25 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->asins = [
-            'B0058DXA4W',
+            'B00G9ZVQ12',
+            'B004ZXYU8Q',
         ];
         $this->baseUrl = "http://www.amazon.it/gp/product/";
         $this->storageRoom = __DIR__ . "/storage/";
+    }
+
+    public function tearDown()
+    {
+        exec("rm -Rf " . $this->storageRoom . "*");
     }
 
     public function testCrawlerIsSavingLocallyWebPages()
     {
         $expectedFilename = $this->asins[0] . "-" . time() . ".html"; //ASIN-123456.html
 
-        $crawler = new Crawler($this->baseUrl);
-        $crawler->setStorageFolder($this->storageRoom)
-                ->run($this->asins);
+        $crawler = new Crawler($this->baseUrl, $this->storageRoom);
+        $files = $crawler->run($this->asins);
 
-        $this->assertTrue(file_exists($this->storageRoom . $expectedFilename));
+        $this->assertEquals(2, count($files));
     }
 }

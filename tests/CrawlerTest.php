fbdd29f631d44ba27eec16b3324f901d549b3be9
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-02-28 21:35:16 +0000

    Better test

--- tests/CrawlerTest.php
+++ tests/CrawlerTest.php
@@ -7,20 +7,20 @@ class CrawlerTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->asins = [
-            'B008EOJIVG',
+            'B0058DXA4W',
         ];
-        // http://amazon.it/gp/product/
-        $this->fakeAmazonUrl = __DIR__ . "/fixtures/"; 
+        $this->baseUrl = "http://www.amazon.it/gp/product/";
         $this->storageRoom = __DIR__ . "/storage/";
     }
 
     public function testCrawlerIsSavingLocallyWebPages()
     {
-        $crawler = new Crawler($this->fakeAmazonUrl);
+        $expectedFilename = $this->asins[0] . "-" . time() . ".html"; //ASIN-123456.html
+
+        $crawler = new Crawler($this->baseUrl);
         $crawler->setStorageFolder($this->storageRoom)
                 ->run($this->asins);
 
-        $expectedFilename = $this->asins[0] . "-" . time() . ".html"; //ASIN-123456.html
         $this->assertTrue(file_exists($this->storageRoom . $expectedFilename));
     }
 }

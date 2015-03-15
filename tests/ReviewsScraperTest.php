1bd5a57e8b90980c2f46d8cc81989f284e65caab
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-03-15 17:01:09 +0000

    Adding stubbed response for reviews scraping

--- tests/ReviewsScraperTest.php
+++ tests/ReviewsScraperTest.php
@@ -23,17 +23,43 @@ class ReviewsScraperTest extends \PHPUnit_Framework_TestCase
         $this->collection->remove([]);
     }
 
-    public function testCanScrapeProductPageAndPersistReviews()
+    public function test_can_scrape_product_page_and_persist_reviews()
     {
+        $stubbedHtml = file_get_contents(__DIR__ . "/fixtures/html/reviews.html");
+
+        // Mocking guzzle
+        $client = $this->getMockBuilder('GuzzleHttp\Client')
+                       ->disableOriginalConstructor()
+                       ->getMock();
+        $request = $this->getMockBuilder('GuzzleHttp\Message\Request')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+        $response = $this->getMockBuilder('GuzzleHttp\Message\Response')
+                         ->disableOriginalConstructor()
+                         ->getMock();
+        $htmlStream = $this->getMockBuilder('GuzzleHttp\Stream\Stream')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+
+        $client->method('createRequest')
+               ->willReturn($request);
+        $client->method('send')
+               ->willReturn($response);
+        $response->method('getBody')
+                 ->willReturn($stubbedHtml);
         $reviewsScraper = new ReviewsScraper(
             $this->environment,
-            new Client()
+            $client
         );
 
         $reviewsScraper->run([
-            'B00RXILJ16',
+            'a-text-asin',
         ]);
 
-        $this->assertEquals(36, $this->collection->count());
+        $this->assertEquals(10, $this->collection->count());
+    }
+
+    public function test_scraper_should_run_only_if_md5_has_changed()
+    {
     }
 }

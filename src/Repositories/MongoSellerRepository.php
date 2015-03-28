274877e10bd09c13596f6b68711e8f494b2d3b4b
Jacopo Nardiello <jacopo@workdigital.co.uk>
2015-03-28 12:46:52 +0000

    Adding SellerRepository

---
+++ src/Repositories/MongoSellerRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+* This file is part of Work Digital's Data Platform.
+*
+* (c) 2015 Work Digital
+*/
+
+namespace Simian\Repositories;
+
+use Prophecy\Exception\Prediction\AggregateException;
+use Simian\Environment\Environment;
+
+/**
+* Class MongoSellerRepository
+*
+* @author Jacopo Nardiello <jacopo.nardiello@dice.com>
+*/
+class MongoSellerRepository
+{
+    private $collection;
+
+    public function __construct(Environment $environment)
+    {
+        $client = new \MongoClient($environment->get('mongo.host'));
+        $db = $client->selectDB($environment->get('mongo.data.db'));
+
+        $this->collection = $db->selectCollection($environment->get('mongo.merchants'));
+    }
+
+    public function findName($sellerId)
+    {
+        $data = $this->collection->findOne([
+            '_id' => $sellerId,
+        ]);
+
+        return $data['name'];
+    }
+
+    public function findEmail($sellerId)
+    {
+        $data = $this->collection->findOne([
+            '_id' => $sellerId,
+        ]);
+
+        return $data['email'];
+    }
+
+    public function insertOne(array $sellerData)
+    {
+        $this->sanitizeData($sellerData);
+
+        $this->collection->insert($sellerData);
+    }
+
+    public function remove(array $data)
+    {
+        $this->collection->remove($data);
+    }
+
+    public function findSellerData($sellerId)
+    {
+        return [
+            '_id' => $sellerId,
+            'name' => $this->findName($sellerId),
+            'email' => $this->findEmail($sellerId)
+        ];
+    }
+
+    private function sanitizeData($data)
+    {
+        if (!isset($data['_id'], $data['name'], $data['email'])) {
+            throw new \Exception('Malformed data when adding new seller');
+        }
+    }
+}
---
+++ tests/Repositories/MongoSellerRepositoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+* This file is part of Work Digital's Data Platform.
+*
+* (c) 2015 Work Digital
+*/
+
+namespace Simian\Repositories;
+
+use Simian\Environment\Environment;
+
+/**
+* Class MongoSellerRepositoryTest
+*
+* @author Jacopo Nardiello <jacopo.nardiello@dice.com>
+*/
+class MongoSellerRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->environment = new Environment('test');
+        $this->repository = new MongoSellerRepository($this->environment);
+    }
+
+    protected function tearDown()
+    {
+        $this->repository->remove([]);
+    }
+
+    public function test_repository_can_find_seller_data()
+    {
+        $this->loadMinotaurFixtures();
+        $sellerId = 'A3RFFOCMGATC6W';
+        $expectedSellerData = [
+            '_id' => 'A3RFFOCMGATC6W',
+            'name' => 'Minotaur',
+            'email' => 'callum@mediadevil.com',
+        ];
+
+        $this->assertEquals($expectedSellerData, $this->repository->findSellerData($sellerId));
+    }
+
+    private function loadMinotaurFixtures()
+    {
+        $minotaurData = [
+            '_id' => 'A3RFFOCMGATC6W',
+            'name' => 'Minotaur',
+            'email' => 'callum@mediadevil.com'
+        ];
+
+        $this->repository->insertOne($minotaurData);
+    }
+}

1f452aec794c9805b945869437639d209e18c34f
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-05-30 07:18:39 -0400

    Updated us template for latest layout

--- templates/us.php
+++ templates/us.php
@@ -1,16 +1,16 @@
 <?php
 
 return $template = [
-    "_id" => "(//div/span/a/@href)[1]",
-    "rating" => "(//div//span/@class)[1]",
-    "product_title" => "(//div/b)[1]",
-    "product_link" => "(//div/b/a/@href)[1]",
-    "review_title" => "(//b)[1]",
-    "review_author" => "(//div/a[1])[1]",
-    "date" => "(//nobr)[1]",
-    "verified_purchase" => "//span[@class='crVerifiedStripe']",
+    "_id" => "(//div[@id='cm_cr-review_list']/div/@id)[1]",
+    "rating" => "(//i/span)[1]",
+    "product_title" => "(//a[contains(concat(' ', @class, ' '), ' a-size-mini ')])[1]",
+    "product_link" => "(//a[contains(concat(' ', @class, ' '), ' a-size-mini ')]/@href)[1]",
+    "review_title" => "(//a[contains(concat(' ', @class, ' '), ' review-title ')])[1]",
+    "review_author" => "(//div[contains(concat(' ', @class, ' '), ' review ')]//div[@class='a-row']//a[contains(concat(' ', @class, ' '), ' author ')])[1]",
+    "date" => "(//div[contains(concat(' ', @class, ' '), ' review ')]//span[contains(concat(' ', @class, ' '), ' review-date ')])[1]",
+    "verified_purchase" => "(//span[@class='a-declarative']/a/span)[1]",
     "permalink" => "(//div/span/a/@href)[1]",
-    "text" => "//div[@class='reviewText']",
-    "main_product_link" => "(//h1/div/a/@href)[1]",
-    "next" => "(//span[@class='paging']/a[contains(text(), 'Next ›')]/@href)[1]",
+    "text" => "//span[contains(concat(' ', @class, ' '), ' review-text ')]",
+    "main_product_link" => "(//div[contains(concat(' ', @class, ' '), ' product-title ')]//a/@href)[1]",
+    "next" => "(//ul[@class='a-pagination']//li[@class='a-last']/a/@href)[1]",
 ];
